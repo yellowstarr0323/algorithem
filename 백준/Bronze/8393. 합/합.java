fa97b0e83546ef463097e5455f0fa703dc949716
hansaem <ihansaem60@gmail.com>
2025-08-06 01:26:06 +0900

    [Bronze V] Title: 합, Time: 172 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,21 @@
+import javax.security.auth.login.AccountException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+        
+
+
+
+
+    }
+}
