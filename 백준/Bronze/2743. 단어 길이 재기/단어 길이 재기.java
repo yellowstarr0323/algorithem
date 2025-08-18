eac19b01773bedda765a0e2198685d165e1f5679
hansaem <ihansaem60@gmail.com>
2025-08-18 17:00:29 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    System.out.println(name.length());
+
+  }
+}
