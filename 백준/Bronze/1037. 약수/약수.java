2e30d2385882d792b79d2175df06821d7a2efce7
hansaem <ihansaem60@gmail.com>
2025-09-21 19:08:35 +0900

    [Bronze I] Title: 약수, Time: 172 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int result = arr[0] * arr[n-1];
+        System.out.println(result);
+    }
+}
