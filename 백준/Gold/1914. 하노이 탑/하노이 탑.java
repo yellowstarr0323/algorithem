68c6e20e9cde193461f5fa20167b891838f6b46d
yellowstarr0323 <ihansaem60@gmail.com>
2025-10-22 23:19:33 +0900

    [Gold V] Title: 하노이 탑, Time: 368 ms, Memory: 48180 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.java
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.java
@@ -1,37 +1,30 @@
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.math.BigInteger;
+import java.util.Scanner;
 
-public class Main {
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static int N = 0;
-
-  public static void main(String[] args) throws Exception {
-
-    
-    N = Integer.parseInt(br.readLine());
-
-    System.out.println(BigInteger.TWO.pow(N).subtract(BigInteger.ONE));
-
-    if (N <= 20) {
-      hanoi(N, 1, 2, 3);
+public class Main{
+  public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(BigInteger.TWO.pow(n).subtract(BigInteger.ONE));
+    if(n<=20){
+      hanoi(n,1,2,3);
     }
-
     bw.flush();
     bw.close();
-    br.close();
+
   }
-
-  public static void hanoi(int n, int from, int tmp, int to) throws IOException{
-
-    if (n == 1) {
-      bw.write(from + " " + to + "\n"); 
+  public static void hanoi(int n, int start, int mid, int end) throws IOException {
+    if(n == 0){
       return;
     }
+    hanoi(n-1, start, end, mid);
+    bw.write(start+" "+end+"\n");
+    hanoi(n-1, mid, start, end);
 
-    hanoi(n - 1, from, to, tmp);
-    bw.write(from + " " + to + "\n"); 
 
-    hanoi(n-1, tmp, from, to);
   }
 }
