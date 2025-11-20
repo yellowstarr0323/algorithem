33e5c0b8f11e632585f81062b8cb89f918b00f9d
yellowstarr0323 <ihansaem60@gmail.com>
2025-11-20 19:28:59 +0900

    [Gold V] Title: 로봇 청소기, Time: 228 ms, Memory: 20924 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
     int n = sc.nextInt();
     int m = sc.nextInt();
     int[][] arr = new int[n][m];
@@ -17,52 +16,46 @@ public class Main {
         arr[i][j] = sc.nextInt();
       }
     }
-
     arr[x][y] = 2;
-    int[] dx={-1,0,1,0};
-    int[] dy={0,1,0,-1};
+
     while(true){
 
-
-      boolean move = false;
-
+      boolean canMove = false;
+      int[] dx = {-1,0,1,0};
+      int[] dy = {0,1,0,-1};
       for (int i = 0; i < 4; i++) {
-        direction = (direction +3)%4;
-        int nx = x + dx[direction];
-        int ny = y + dy[direction];
-
-        if(nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] == 0){
+        direction = (direction + 3)%4;
+        int nx = x+dx[direction];
+        int ny = y+dy[direction];
+        if(nx>=0 && nx<n && ny>=0 && ny<m && arr[nx][ny]==0){
+          canMove = true;
           x=nx;
           y=ny;
-          move = true;
-          arr[nx][ny] = 2;
+          arr[x][y] = 2;
           break;
         }
       }
-      if(!move){
+      if(!canMove){
         int back = (direction+2)%4;
-        int bx = x + dx[back];
-        int by = y + dy[back];
-
-        if(bx>=0 && bx<n && by>=0 && by<m && arr[bx][by] !=1){
+        int bx = x+dx[back];
+        int by = y+dy[back];
+        if(bx>=0 && bx<n && by>=0 && by<m&&arr[bx][by]!=1){
           x=bx;
           y=by;
         }
-        else {
+        else
           break;
-        }
       }
-
     }
-    int count=0;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if(arr[i][j] == 2){
+    int count = 0;
+    for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+        if(arr[i][j]==2){
           count++;
         }
       }
     }
     System.out.println(count);
-  }
 
+  }
 }
